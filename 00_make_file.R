94b88e1b42f833fb36f85d9fae893c6d72afe8a8
EvaMaeRey <evangeline.mae@gmail.com>
2019-03-10 20:11:38 -0600

    Added "make" file.

---
+++ 00_make_file.R
@@ -0,0 +1,9 @@
+# Execute this code to run all source code
+knitr::knit("01_data_cleaning.Rmd")
+knitr::knit("02_analysis.Rmd")
+knitr::knit("10_paper.Rmd")
+knitr::knit("11_poster_professional.Rmd")
+knitr::knit("12_presentation_slides.Rmd")
+
+
+
