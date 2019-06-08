06dd203be1154a4e392e688d5809add461c3dc7b
EvaMaeRey <evangeline.mae@gmail.com>
2019-06-08 16:49:41 -0600

    Adding getting data step to procedure.

--- 00_make_file.R
+++ 00_make_file.R
@@ -1,6 +1,7 @@
 # Execute this code to run all source code
-rmarkdown::render("01_data_cleaning.Rmd")
-rmarkdown::render("02_analysis.Rmd")
+rmarkdown::render("01_getting_data.Rmd")
+rmarkdown::render("02_data_cleaning.Rmd")
+rmarkdown::render("03_analysis.Rmd")
 rmarkdown::render("10_paper.Rmd")
 rmarkdown::render("11_poster_professional.Rmd")
 rmarkdown::render("12_presentation_slides.Rmd")
